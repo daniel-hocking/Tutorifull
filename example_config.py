47456d1ab4ce7aa5f80fecdc0f7e18c3e7775177
Chybby <achybby@gmail.com>
2016-03-13 01:03:07 -0800

    change the config file in the repo to an example

---
+++ example_config.py
@@ -0,0 +1,6 @@
+# This is an example config file
+# copy this into a file called 'config.py' and change values to ones specific to you
+
+DATABASE = 'tutorifull.db'          # the name of the file to save the sqlite database to
+DEBUG = True                        # never set this to True in production
+SECRET_KEY = 'example secret key'   # flask secret key
